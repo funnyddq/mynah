1bb36ad591635625e7e1a949017c74ffa7b7580e
funnyddq <funnyddq@gmail.com>
2017-04-21 00:18:11 +0800

    2017年 04月 21日 星期五 00:18:11 CST

--- src/mynah.c
+++ src/mynah.c
@@ -1,11 +1,51 @@
 #include <config.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int
-main()
+parseCommandLine(int argc, char *argv[]);
+
+int
+main(int argc, char *argv[])
 {
+	int ret;
+
 	printf("Hello, world!\n");
 	printf("This is " PACKAGE_STRING ".\n");
+	ret = parseCommandLine(argc, argv);
+	if (ret != 0)
+	{
+		fprintf(stderr, "failed!\n");
+		return EXIT_FAILURE;
+	}
+	printf("OK\n");
 	return EXIT_SUCCESS;
+}
+
+int
+parseCommandLine(int argc, char *argv[])
+{
+	int flags, opt;
+	int nsecs, tfnd;
+
+	nsecs = 0;
+	tfnd = 0;
+	flags = 0;
+	while ((opt = getopt(argc, argv, "nt:")) != -1) {
+		switch (opt) {
+		case 'n':
+			flags = 1;
+			break;
+		case 't':
+			nsecs = atoi(optarg);
+			tfnd = 1;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n", argv[0]);
+			return 1;
+		}
+	}
+	return 0;
 }
